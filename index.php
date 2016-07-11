4523389edce4d7e807c7f82b5fae2ff270420334
Mohaimenul Adnan <adnan007.id@gmail.com>
2016-07-11 11:09:27 +0600

    Initial commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Zoom Composer
+Plugin URI: http://zoomlookbook.com/
+Description: Visual composer integration for ajax-zoom
+Version: 0.1
+Author: Mohaimenul Adnan
+Author URI: http://eadnan.com
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
